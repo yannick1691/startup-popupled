cb21da73ec855fc2427b4b10c98d0f87cf3fc201
Yannick <yannick.frisart@outlook.com>
2017-01-18 14:25:17 +0100

    - added over ons

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,38 @@
 $(document).ready(function () {
   const wIPopup = document.querySelector('#watispopupled');
+  //initialize swiper when document ready  
+  var mySwiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: '2',
+    spaceBetween: 10,
+    autoplay: 5000,
+    speed: 400,
+    coverflow: {
+      rotate: 30,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true
+    },
+    breakpoints: {
+      // when window width is <= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is <= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      // when window width is <= 640px
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    }
+  });
 });
 //# sourceMappingURL=script.js.map
