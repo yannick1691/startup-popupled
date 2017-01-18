0c2c003228fc72550c6025845af01caabedbffc5
Yannick <yannick.frisart@outlook.com>
2017-01-18 21:50:42 +0100

    -Added Waarom ons

--- dev/js/script.js
+++ dev/js/script.js
@@ -3,18 +3,20 @@ $(document).ready(function () {
   //initialize swiper when document ready  
   var mySwiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
+    initialSlide: 1,
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: '2',
     spaceBetween: 10,
-    autoplay: 5000,
+    loop: true,
+    loopAdditionalSlides: 1,
     speed: 400,
     coverflow: {
-      rotate: 30,
+      rotate: 60,
       stretch: 0,
       depth: 100,
       modifier: 1,
-      slideShadows: true,
+      slideShadows: false,
     },
     breakpoints: {
         // when window width is <= 320px
@@ -25,7 +27,7 @@ $(document).ready(function () {
         // when window width is <= 480px
         480: {
           slidesPerView: 2,
-          spaceBetween: 30
+          spaceBetween: 20
         },
         // when window width is <= 640px
         640: {
@@ -34,4 +36,43 @@ $(document).ready(function () {
         }
       }
   });
+  var onzeKlanten = new Swiper('.onzeKlanten-container', {
+    effect: 'coverflow',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    grabCursor: false,
+    centeredSlides: true,
+    slidesPerView: '3',
+    spaceBetween: 0,
+    autoplay: 2500,
+    autoplayDisableOnInteraction: false,
+    loop: true,
+    loopAdditionalSlides: 1,
+    speed: 500,
+    coverflow: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : false,
+    },
+    breakpoints: {
+        // when window width is <= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 60
+        },
+        // when window width is <= 480px
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 60
+        },
+        // when window width is <= 640px
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 60
+        }
+      }
+  });
+  onzeKlanten.disableTouchControl();
 });
