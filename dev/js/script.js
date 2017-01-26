0d09a41d1e456ce33b080bbeed8d0855c066c8c4
Yannick <yannick.frisart@outlook.com>
2017-01-26 22:46:24 +0100

    - Coded Offerte and generating
    Made the generator and added all the calculators also now use jquery ui instead of input type="week" because of lack of safari support

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   const wIPopup = document.querySelector('#watispopupled');
   scrollfixnav();
-
   //smoothscroll
     $('.flex-nav a[href^="#"]').on('click', function (e) { // kijkt naar click op een nav element
         e.preventDefault(); // stop browser van het toevoegen van # aan url
@@ -307,15 +306,194 @@ function waaromOnsRotation(e) {
 
 
 // Function to generate a pdf 
-function offerte() {
-  var offerte = new jsPDF()
+const offerteForm = document.querySelector('.offerte');
 
-  offerte.text('Hello world!', 10, 10)
-  offerte.autoPrint()
-  offerte.addPage()
+function offerte(e) {
+  // voorkom reload on submit
+  e.preventDefault();
+
+  // Variables die ik nodig heb in invoice
+
+  // invoice bedrijf gegegevens
+  let naamTarget = document.querySelector('#offnaam').value;
+  let emailTarget = document.querySelector('#emailoff').value;
+  let telTarget = document.querySelector('#teloff').value;
+  let companyTarget = document.querySelector('#companyoff').value;
+
+  // invoice datum gegegevens
+  const month = new Array();
+    month[0] = "januari";
+    month[1] = "februari";
+    month[2] = "maart";
+    month[3] = "april";
+    month[4] = "mei";
+    month[5] = "juni";
+    month[6] = "juli";
+    month[7] = "augustus";
+    month[8] = "september";
+    month[9] = "oktober";
+    month[10] = "november";
+    month[11] = "december";
+  let currentDate = new Date();
+  let currentMonth = month[currentDate.getMonth()];
+  let currentDay = currentDate.getDay();
+  let currentYear = currentDate.getFullYear();
+  let currentOffDate = currentDay + ' ' + currentMonth + ' ' + currentYear;
+
+
+  // invoice prijs berekenen
+  let waardeTarget = document.querySelector('#locatieoff').value;
+  let startDatumTarget = document.querySelector('#start-date').value;
+  let startDatumTarget2 = startDatumTarget.split(',');
+  let startDatumTarget3 = parseFloat(startDatumTarget2[1]);
+  let endDatumTarget = document.querySelector('#end-date').value;
+  let endDatumTarget2 = endDatumTarget.split(',');
+  let endDatumTarget3 = parseFloat(endDatumTarget2[1]);
+
+  console.log(startDatumTarget2);
+  let startDatumTargetW = parseFloat(startDatumTarget2[0].replace("Week ", ""));
+  let endDatumTargetW = parseFloat(endDatumTarget2[0].replace("Week ", ""));
+
+  console.log(startDatumTarget3, startDatumTargetW);
+  console.log(endDatumTarget3, endDatumTargetW);
+  
+  let hiredYearsWeeks = (endDatumTarget3 - startDatumTarget3) * 52;
+
+  let weeksHired = (endDatumTargetW - startDatumTargetW) + hiredYearsWeeks;
+  if (weeksHired <= 0) {
+    window.alert('Je moet minimaal 1 week een scherm huren')
+    return false;
+  }
+
+  // random 'offerte number' generator
+  let offerteNummer = Math.round(Math.random() * 2323030000);
+  console.log(offerteNummer);
+
+  // calculate price
+  let totalWeeksPrice1 = weeksHired * waardeTarget;
+  let totalWeeksPrice = +(Math.round(totalWeeksPrice1 + "e+2") + "e-2");
+
+  // Taxes
+  let totalTax1 = totalWeeksPrice / 100 * 21;
+  let totalTax = +(Math.round(totalTax1 + "e+2") + "e-2");
+
+  // Total price inclusief Taxes
+  let totalPriceTax1 = totalWeeksPrice + totalTax;
+  let totalPriceTax = +(Math.round(totalPriceTax1 + "e+2") + "e-2");
+
+  // location name
+  console.log(waardeTarget);
+  let locationName = 0;
+    if (waardeTarget == 99.99) {
+      locationName = 'Korte Lijnbaan';
+    } else if (waardeTarget == 109.99) {
+      locationName = 'Stadhuisplein';
+    } else if (waardeTarget == 89.99) {
+      locationName = 'het Weena';
+    }
+  
+    let locationNameOff = locationName;
+  
+
+
+  // functie om de pdf te maken
+  var doc = new jsPDF('a4');
+  
+  doc.setFontSize(46);
+  doc.setTextColor(229, 39, 45);
+  doc.text('popupled', 15, 30);
+
+  doc.setFontSize(15)
+  doc.setTextColor(54, 174, 189)
+  doc.text('offerte', 15, 60)
+  doc.setTextColor(229, 39, 45);
+  doc.text('NOT PAID', 85, 60)
+  doc.setLineWidth(0.1)
+  doc.setDrawColor(210)
+  doc.line(10, 62, 200, 62); // horizontal line
+  doc.line(10, 85, 200, 85); // horizontal line
+  doc.line(10, 110, 200, 109); // horizontal line
+  doc.line(10, 134, 200, 134); // horizontal line
+
+  doc.setFontSize(12)
+  doc.setTextColor(200)
+  doc.text('tel: 06-10520429\remail: info@popupled.nl\radress: Meikeverstraat 9 A02 rotterdam\rKVK-nummer: 61836680', 120, 20);
+  doc.setTextColor(122)
+  doc.setFontType('bolditalic')
+  doc.text(`${currentOffDate}`, 155, 60)
+
+  doc.setTextColor(120)
+  doc.setFontType('normal')
+  doc.text(`naam: \remail: \rtel: \rbedrijf: `, 18, 67)
+  doc.text(`${naamTarget}\r${emailTarget}\r${telTarget}\r${companyTarget}`, 52, 67)
+
+  doc.setFillColor(100)
+  doc.rect(0, 86, 300, 10, 'f')
+  doc.setFontType('bold');
+  doc.setTextColor(54, 174, 189)
+  doc.text(`locatie`, 15, 92)
+  doc.text(`aantal weken`, 70, 92)
+  doc.text(`week prijs`, 130, 92)
+  doc.text(`total`, 180, 92)
+
+  doc.setFontType('normal')
+  doc.setTextColor(20)
+  doc.text(`${locationName}`, 16, 103)
+  doc.text(`${weeksHired}`, 71, 103)
+  doc.text(`€${waardeTarget}`, 131, 103)
+  doc.text(`€${totalWeeksPrice}`, 181, 103)
+
+  doc.text(`subtotal`, 131, 115)
+  doc.text(`€${totalWeeksPrice}`, 181, 115)
+  doc.text(`btw procent`, 131, 122)
+  doc.text(`21%`, 181, 122)
+  doc.text(`btw`, 131, 129)
+  doc.text(`€${totalTax}`, 181, 129)
+  doc.text(`total price`, 131, 140)
+  doc.text(`€${totalPriceTax}`, 181, 140)
+  
+  doc.setFontType('bold')
+  doc.setTextColor(54, 174, 189)
+  doc.text(`offerte ID: #${offerteNummer}`, 15, 140)
+
+  doc.setTextColor(40)
+  doc.text(`*** disclaimer ***`, 85, 190)  
+  doc.setFontType('normal')
+  doc.setTextColor(30)
+  doc.text(`This offerte is in no way binding and popupled can deny or end partnership at any time\rThis offerte was created on ${currentOffDate}\rFor discussing prices or more information please contact info@popupled.nl\rPrices may change at anytime and are not binding.`, 15, 200)
+
+  doc.setProperties({
+    title: 'offerte popupled',
+    subject: 'Offerte voor Popupled',
+    author: 'popupled',
+    keywords: 'offerte, popupled, huren, ledscherm'
+  });
+
+  doc.output('dataurlnewwindow')
 }
 
-// offerte();
+
+// Function om tussen contact en offerte te switchen
+const contactFormButton = document.querySelector('button[data-button="contact"]');
+const offerteFormButton = document.querySelector('button[data-button="offerte"]');
+const offerteFormulier = $('.offerteForm');
+const contactFormulier = $('.contactForm');
+
+function contactFadeIn() {
+  offerteFormulier.fadeOut(500);
+  setTimeout(function() {
+    contactFormulier.fadeIn(500);
+  }, 450);
+}
+
+function offerteFadeIn() {
+  contactFormulier.fadeOut(500);
+  setTimeout(function() {
+    offerteFormulier.fadeIn(500)
+  }, 450);
+}
+
+
 
 //Event listeners
 document.addEventListener('scroll', debounce(onScroll, 10));
@@ -325,3 +503,6 @@ $(window).resize(resizeFix);
 ontdekOnsBut.addEventListener('click', ontdekOns);
 landingPage.addEventListener('mousemove', popupledLogoShadow);
 waaromOnsDiv.addEventListener('mousemove', debounce(waaromOnsRotation, 3));
+offerteForm.addEventListener('submit', offerte);
+contactFormButton.addEventListener('click', contactFadeIn);
+offerteFormButton.addEventListener('click', offerteFadeIn);
